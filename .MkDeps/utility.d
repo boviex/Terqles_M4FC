caf3d661a2ca80856cfefa36c86b4ba44443db5b
Bovie Xiong <bovie.xiong@gmail.com>
2018-02-23 11:45:26 +0000

    forgot to include lyn

---
+++ .MkDeps/utility.d
@@ -0,0 +1,82 @@
+src/utility.o src/utility.asm: src/utility.c src/utility.h \
+ src/libgbafe/gbafe.h \
+ c:\devkitpro\devkitarm\lib\gcc\arm-none-eabi\7.1.0\include\stdint.h \
+ c:\devkitpro\devkitarm\arm-none-eabi\include\stdint.h \
+ c:\devkitpro\devkitarm\arm-none-eabi\include\machine\_default_types.h \
+ c:\devkitpro\devkitarm\arm-none-eabi\include\sys\features.h \
+ c:\devkitpro\devkitarm\arm-none-eabi\include\_newlib_version.h \
+ c:\devkitpro\devkitarm\arm-none-eabi\include\sys\_intsup.h \
+ c:\devkitpro\devkitarm\arm-none-eabi\include\sys\_stdint.h \
+ src/libgbafe/gbafe/random.h src/libgbafe/gbafe/common.h \
+ src/libgbafe/gbafe/proc.h src/libgbafe/gbafe/animhandle.h \
+ src/libgbafe/gbafe/class.h src/libgbafe/gbafe/character.h \
+ src/libgbafe/gbafe/game.h src/libgbafe/gbafe/item.h \
+ src/libgbafe/gbafe/unit.h src/libgbafe/gbafe/character.h \
+ src/libgbafe/gbafe/class.h src/libgbafe/gbafe/unit.h \
+ src/libgbafe/gbafe/map.h src/libgbafe/gbafe/menu.h \
+ src/libgbafe/gbafe/levelup.h src/libgbafe/gbafe/standunit.h \
+ src/libgbafe/gbafe/moveunit.h src/libgbafe/gbafe/proc.h \
+ src/libgbafe/gbafe/animhandle.h src/libgbafe/gbafe/trap.h \
+ src/libgbafe/gbafe/action.h src/libgbafe/gbafe/other.h
+
+src/utility.h:
+
+src/libgbafe/gbafe.h:
+
+c:\devkitpro\devkitarm\lib\gcc\arm-none-eabi\7.1.0\include\stdint.h:
+
+c:\devkitpro\devkitarm\arm-none-eabi\include\stdint.h:
+
+c:\devkitpro\devkitarm\arm-none-eabi\include\machine\_default_types.h:
+
+c:\devkitpro\devkitarm\arm-none-eabi\include\sys\features.h:
+
+c:\devkitpro\devkitarm\arm-none-eabi\include\_newlib_version.h:
+
+c:\devkitpro\devkitarm\arm-none-eabi\include\sys\_intsup.h:
+
+c:\devkitpro\devkitarm\arm-none-eabi\include\sys\_stdint.h:
+
+src/libgbafe/gbafe/random.h:
+
+src/libgbafe/gbafe/common.h:
+
+src/libgbafe/gbafe/proc.h:
+
+src/libgbafe/gbafe/animhandle.h:
+
+src/libgbafe/gbafe/class.h:
+
+src/libgbafe/gbafe/character.h:
+
+src/libgbafe/gbafe/game.h:
+
+src/libgbafe/gbafe/item.h:
+
+src/libgbafe/gbafe/unit.h:
+
+src/libgbafe/gbafe/character.h:
+
+src/libgbafe/gbafe/class.h:
+
+src/libgbafe/gbafe/unit.h:
+
+src/libgbafe/gbafe/map.h:
+
+src/libgbafe/gbafe/menu.h:
+
+src/libgbafe/gbafe/levelup.h:
+
+src/libgbafe/gbafe/standunit.h:
+
+src/libgbafe/gbafe/moveunit.h:
+
+src/libgbafe/gbafe/proc.h:
+
+src/libgbafe/gbafe/animhandle.h:
+
+src/libgbafe/gbafe/trap.h:
+
+src/libgbafe/gbafe/action.h:
+
+src/libgbafe/gbafe/other.h:
